e5ef6ff07821b095280a35e33718d7f415f54091
Abhishek Singh <abhishek.skd13@gmail.com>
2024-06-16 19:37:37 +0530

    updated

--- factorial.cpp
+++ factorial.cpp
@@ -1,18 +1,1 @@
-
-#include <iostream>  
-using namespace std;
-int main()
-{
-  int n, fac;
-  cout<<"\n enter the given the number: ";
-  cin>>n;
-  fac=1;
-  while(n>=1){
-    fac = fac*n;
-    n = n-1;
-  }
-  cout<<"\n factorial:"<<fac;
-  return 0;
-}
-
  
